a6c5e44b5d29f2ec01c94c61dcaf69fbb4dc2e41
ropbear <github@selfhosted.systems>
2025-08-04 19:42:45 -0400

    building pretext tests

--- ophishal/pretext/pretext.py
+++ ophishal/pretext/pretext.py
@@ -17,32 +17,34 @@ class Pretext(BaseConfig):
         "phish_type":str,
         "email_subject":str,
         "email_body":str,
-        "selected_template":str,
+        "template":str,
         "attachment":str
     }
     def parse(self, config:dict):
 
-        self.source = PretextSource(**config["source"])
-        self.targets = [Target(**t) for t in config["targets"]]
+        self.source = PhishSource(**config["source"])
+        self.targets = [PhishTarget(**t) for t in config["targets"]]
 
         self.campaign_identifier    = config["campaign_identifier"]
         self.vector                 = config["vector"]
         self.persuasion_mechanisms  = config["persuasion_mechanisms"]
+        self.language               = config["language"]
         self.language_style         = config["language_style"]
         self.topic                  = config["topic"]
         self.phish_type             = config["phish_type"]
         self.email_subject          = config["email_subject"]
         self.email_body             = config["email_body"]
-        self.selected_template      = config["selected_template"]
+        self.template               = Path(config["template"])
+        self.attachment             = Path(config["attachment"])
 
 
 @dataclass
-class PretextSource:
+class PhishSource:
     type: str
     uid: str
 
 
 @dataclass
-class Target:
+class PhishTarget:
     type: str
     uid: str
--- tests/test_pretext.py
+++ tests/test_pretext.py
@@ -2,12 +2,79 @@
 import json
 import pytest
 from pathlib import Path
-from ophishal.pretext.pretext import Pretext
+from ophishal.pretext.pretext import Pretext, PhishSource, PhishTarget
 
 @pytest.fixture
 def generate_pretext():
     testfile = Path(__file__).resolve().parent.parent / "examples" / "pretext.json"
     return Pretext(filepath=testfile)
 
-def test_pretext(generate_pretext):
-    pass
+def test_pretext_campaign_id(generate_pretext):
+    ptx = generate_pretext
+    assert ptx.campaign_identifier == "sptg-q3-finance-credential-harvest"
+    assert isinstance(ptx.campaign_identifier, str)
+
+def test_pretext_vector(generate_pretext):
+    ptx = generate_pretext
+    assert ptx.vector == "email"
+    assert isinstance(ptx.vector, str)
+
+def test_pretext_source(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.source, PhishSource)
+    assert ptx.source.type == "employee"
+    assert isinstance(ptx.source.type, str)
+    assert ptx.source.uid == "emp_randy"
+    assert isinstance(ptx.source.uid, str)
+
+def test_pretext_targets(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.targets, list)
+    assert all([isinstance(tgt, PhishTarget) for tgt in ptx.targets])
+    assert ptx.targets[0].type == "employee"
+    assert ptx.targets[1].uid == "emp_cartman"
+
+def test_pretext_persuasion_mechs(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.persuasion_mechanisms, list)
+    assert ptx.persuasion_mechanisms == ["urgency","authority"]
+
+def test_pretext_language(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.language, str)
+    assert ptx.language == "en"
+
+def test_pretext_language_style(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.language_style, list)
+    assert ptx.language_style == ["corporate", "direct", "urgent"]
+
+def test_pretext_topic(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.topic, str)
+    assert ptx.topic == "quarterly financial report access issue"
+
+def test_pretext_phish_type(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.phish_type, str)
+    assert ptx.phish_type == "credential phishing"
+
+def test_pretext_email_subject(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.email_subject, str)
+    assert ptx.email_subject == "Action Required: Q3 Financials Access Expiration"
+
+def test_pretext_email_body(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.email_body, str)
+    assert ptx.email_body == "Hi team,\n\nWe’ve identified an issue with your current permissions to view the Q3 financial documents in our internal system. As of today, your access will expire in 24 hours unless revalidated.\n\nPlease use the secure access portal below to re-authenticate:\n\nhttps://sptg-financials-login.com\n\nThis is time-sensitive and requires completion before COB today.\n\nThanks,\nRandy"
+
+def test_pretext_template(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.template, Path)
+    assert ptx.template == Path("standard-access-urgency.html")
+
+def test_pretext_attachment(generate_pretext):
+    ptx = generate_pretext
+    assert isinstance(ptx.attachment, Path)
+    assert ptx.attachment == Path("invite.ics")
